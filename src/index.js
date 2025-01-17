03718ce167af834980207a91d446998aaaa42676
Truong Tran <trantnce180829@fpt.edu.vn>
2025-01-18 00:14:05 +0700

    cập nhật mới

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './tạo_thử_jsx/renderHTML/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
